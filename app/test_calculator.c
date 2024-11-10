860a2be324e433ddf0918ecb78cc098bab2f6024
Balaji <balajimail@zoho.com>
2024-11-10 15:33:13 +0530

    Apateu, apateu

---
+++ app/test_calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+#include ".\calculator.h"
+
+static void test_add(void);
+
+int main(void)
+{
+    test_add();
+    printf("All test Completed!\n");
+    return 0;
+}
+
+static void test_add(void)
+{
+    assert(add(1, 2) == 3.0);
+    assert(add(1.0, 2.0) == 3.0);
+    assert(add(0, 2.0) == 2.0);
+    assert(add(2.0, 0) == 2.0);
+    assert(add(-4, 2.0) == -2.0);
+}
